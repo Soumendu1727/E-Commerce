dbf1dcba85d5d219f214e0e309196fb138554f1f
Soumendu1727 <sadhukhansoumendu@gmail.com>
2023-08-12 15:31:03 +0530

    Second Commit(Cart Page Added)

--- ecom/src/App.js
+++ ecom/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import Cart from './features/cart/Cart';
+import CartPage from './pages/CartPage';
 
 import {
   createBrowserRouter,
@@ -23,6 +25,11 @@ const router = createBrowserRouter([
   {
     path: '/signup',
     element: <SignupPage></SignupPage>,
+  },
+
+  { 
+    path: '/cart',
+    element: <CartPage></CartPage>,
   },
 ]);
 
--- ecom/src/features/cart/cartSlice.js
+++ ecom/src/features/cart/cartSlice.js
@@ -1,17 +1,16 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './productListAPI';
+import { fetchCount } from './cartAPI';
 
 const initialState = {
   value: 0,
   status: 'idle',
 };
 
-
 export const incrementAsync = createAsyncThunk(
   'counter/fetchCount',
   async (amount) => {
     const response = await fetchCount(amount);
-  
+    // The value we return becomes the `fulfilled` action payload
     return response.data;
   }
 );
@@ -21,7 +20,6 @@ export const counterSlice = createSlice({
   initialState,
   reducers: {
     increment: (state) => {
-     
       state.value += 1;
     },
   },
@@ -38,7 +36,6 @@ export const counterSlice = createSlice({
 });
 
 export const { increment } = counterSlice.actions;
-
 
 export const selectCount = (state) => state.counter.value;
 
