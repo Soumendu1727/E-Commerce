2ebfe9c7175c9106453821d7c28110f815675d5a
Soumendu1727 <sadhukhansoumendu@gmail.com>
2023-08-14 00:47:13 +0530

    Commit 5 Detail page of products

--- ecom/src/App.js
+++ ecom/src/App.js
@@ -5,6 +5,7 @@ import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import CartPage from './pages/CartPage';
 import Checkout from './pages/Checkout';
+import ProductDetailPage from './pages/ProductDetailPage';
 
 import {
   createBrowserRouter,
@@ -34,6 +35,11 @@ const router = createBrowserRouter([
   { 
     path: '/checkout',
     element: <Checkout></Checkout>,
+  },
+
+  { 
+    path: '/product-detail',
+    element: <ProductDetailPage></ProductDetailPage>,
   },
 ]);
 
